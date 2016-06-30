28ebbde951f6bf1be6e59f944d2e9890f0f628c2
Steve Ehrenberg <tehdnite@gmail.com>
2016-06-30 04:10:30 -0500

    Attempting to convert to a carthage framework

---
+++ SWTableViewCellFramework/SWTableViewCellFramework.h
@@ -0,0 +1,25 @@
+//
+//  SWTableViewCellFramework.h
+//  SWTableViewCellFramework
+//
+//  Created by Steve Ehrenberg on 6/30/16.
+//  Copyright © 2016 Chris Wendel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWTableViewCellFramework.
+FOUNDATION_EXPORT double SWTableViewCellFrameworkVersionNumber;
+
+//! Project version string for SWTableViewCellFramework.
+FOUNDATION_EXPORT const unsigned char SWTableViewCellFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWTableViewCellFramework/PublicHeader.h>
+
+
+#import "SWTableViewCell.h"
+#import "SWLongPressGestureRecognizer.h"
+#import "SWCellScrollView.h"
+#import "NSMutableArray+SWUtilityButtons.h"
+#import "SWUtilityButtonTapGestureRecognizer.h"
+#import "SWUtilityButtonView.h"
